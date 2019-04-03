9fcdd6bddaa405c7d344ec1785aa261f05eaae85
klaborde22590 <49076324+klaborde22590@users.noreply.github.com>
2019-04-03 01:57:49 -0400

    Add files via upload

---
+++ Apple.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    public static float bottomY = -20f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if we dropped an apple
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+            // get a pointer to the ApplePicker Script
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        }
+    }
+}
---
+++ ApplePicker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    // Use this for initialization
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>
+                (basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO); // build a list of baskets
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        // get a list of all the apples on the screen
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        // We dropped an apple so get rid of a basket
+        int basketIndex = basketList.Count - 1; // get index of last basket
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        // if we ran out baskets we lose!
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene0");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
